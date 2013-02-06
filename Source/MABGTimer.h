6ea44adf65c0110216b4e0a34e470ce1a0a4f388
Matias Piipari <matias.piipari@gmail.com>
2013-02-06 14:31:05 +0000

    Formatting changes.

--- Source/MABGTimer.h
+++ Source/MABGTimer.h
@@ -10,7 +10,7 @@
 #import <Foundation/Foundation.h>
 
 
-#ifndef mt_dispatch_release
+#ifndef mt_dispatch_strong
     #if TARGET_OS_IPHONE
         #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000
             #define mt_dispatch_release(__v)
@@ -19,7 +19,7 @@
             #define mt_dispatch_release(__v) (dispatch_release(__v));
             #define mt_dispatch_strong assign
         #endif
-        #else
+    #else
         #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
             #define mt_dispatch_release(__v)
             #define mt_dispatch_strong strong
