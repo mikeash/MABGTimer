2892937a6b256461acf5b0e739f701cf039d9a7d
Matias Piipari <matias.piipari@gmail.com>
2016-10-28 00:33:32 +0100

    Adds a framework target.

---
+++ MABGTimerKit/MABGTimerKit.h
@@ -0,0 +1,19 @@
+//
+//  MABGTimerKit.h
+//  MABGTimerKit
+//
+//  Created by Matias Piipari on 28/10/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MABGTimerKit.
+FOUNDATION_EXPORT double MABGTimerKitVersionNumber;
+
+//! Project version string for MABGTimerKit.
+FOUNDATION_EXPORT const unsigned char MABGTimerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MABGTimerKit/PublicHeader.h>
+
+#import "MABGTimer.h"
